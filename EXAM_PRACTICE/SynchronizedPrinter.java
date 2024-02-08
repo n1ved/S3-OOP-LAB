4a30fdda5b5cfff5c2badd0db0516299c098326d
n1ved <nivedrsalini@gmail.com>
2024-02-08 18:19:31 +0530

    Done sync Thread with Swing

---
+++ EXAM_PRACTICE/SynchronizedPrinter.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+import java.awt.event.*;
+public class SynchronizedPrinter {
+    JFrame frame;
+    JTextField textField;
+    JButton button;
+    static int n = 0;
+
+    static synchronized void printDataSync(String data){
+        for(int i=0 ; i<n ; i++){
+            System.out.println(data);
+        }
+    }
+
+    class ThrikkakaraThread extends Thread{
+        public void run(){
+            printDataSync("Thrikkakara");
+        }
+    }
+
+    class KochiThread extends Thread{
+        public void run(){
+            printDataSync("Kochi");
+        }
+    }
+
+    SynchronizedPrinter(){
+        frame = new JFrame();
+        frame.setSize(300,200);
+
+        textField = new JTextField();
+        textField.setBounds(50,0,200,50);
+
+        button = new JButton("Print");
+        button.setBounds(75,50,100,50);
+
+        button.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent ae){
+                    ThrikkakaraThread tt = new ThrikkakaraThread();
+                    KochiThread kt = new KochiThread();
+                    n = Integer.parseInt(textField.getText());
+                    tt.start();
+                    kt.start();
+                }
+            }
+        );
+
+        frame.add(textField);
+        frame.add(button);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SynchronizedPrinter();
+    }
+}
